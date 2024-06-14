043e44eb18cdc1fa2e1b1327260874de8c9d4836
Raghu Reddy <raghured@infinite.com>
2024-06-14 13:15:33 +0530

    commit2

---
+++ hansOn/day2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = a + b;
+            int sub = a - b;
+            int mul = a * b;
+            int div = a / b;
+            Console.WriteLine($"addition is {sum}");
+            Console.WriteLine($"substraction is {sub}");
+            Console.WriteLine($"multiplication is {mul}");
+            Console.WriteLine($"division is {div}");
+            Console.Read();
+
+
+
+
+
+        }
+
+    }
+}
