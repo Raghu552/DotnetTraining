8688e283be8d92de35d7c9e49cde774fe0c76641
Raghu Reddy <raghured@infinite.com>
2024-06-17 00:13:27 +0530

    sumbit

---
+++ C#/assignment/assignment/Swap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    class Swap
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int temp;
+            Console.WriteLine("before swap a is " + a);
+            Console.WriteLine("before swap b is " + b);
+            temp = a;
+            a = b;
+            b = a;
+            b = temp;
+            Console.WriteLine("After swap a is "+a);
+            Console.WriteLine("After swap b is "+b);
+            Console.Read();
+
+        }
+    }
+}
---
+++ C#/assignment/assignment/loop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    class loop
+    {
+       public static void Main()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} {0} {0} {0}",a);
+            Console.WriteLine("{0}{0}{0}{0}", a);
+            Console.WriteLine("{0} {0} {0} {0}", a);
+            Console.WriteLine("{0}{0}{0}{0}", a);
+            Console.Read();
+        }
+        
+    }
+}
