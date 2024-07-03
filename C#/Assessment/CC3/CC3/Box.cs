7ba18cd82452db05b56fd4d20d46f6a329ee8270
Raghu Reddy <raghured@infinite.com>
2024-07-03 12:58:11 +0530

    cc3

---
+++ C#/Assessment/CC3/CC3/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC3
+{
+    public class Box
+    {
+        public double Length { get; set; }
+        public double Breadth { get; set; }
+        public Box(double len, double breadth)
+        {
+            Length = len;
+            Breadth = breadth;
+        }
+        public static Box Add(Box box1, Box box2)
+        {
+            double newLength = box1.Length + box2.Length;
+            double newBreadth = box1.Breadth + box2.Breadth;
+            Box result = new Box(newLength, newBreadth);
+            return result;
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Length: {Length}, Breadth: {Breadth}");
+        }
+    }
+}
---
+++ C#/Assessment/CC3/CC3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC3
+{
+    class Cricket
+    {
+        public static void PointsCalculation(int no_of_matches)
+        {
+            int[] arr = new int[no_of_matches];
+            int sum = 0;
+            for (int i = 0; i < no_of_matches; i++)
+            {
+                Console.WriteLine($"enter the {i + 1} match score");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                sum += arr[i];
+            }
+            Console.WriteLine($"the sum for all scores is {sum}");
+            double avg = sum / no_of_matches;
+            Console.WriteLine($"the average score for all matches is {avg}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number of matches");
+            Cricket.PointsCalculation(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("--------------------");
+            Console.WriteLine("enter the lenght and breadth of box1");
+            Box box1 = new Box(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("enter the lenght and breadth of box2");
+            Box box2 = new Box(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            Box box3 = Box.Add(box1, box2);
+            Console.WriteLine("Details of Box 3:");
+            box3.DisplayDetails();
+            Console.Read();
+        }
+    }
+}
